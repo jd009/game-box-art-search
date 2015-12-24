6ef30a97704c35f74b9014a512dd44a154744c47
John D <deblanc.john@gmail.com>
2015-12-24 01:22:46 -0600

    Added bootstrap styling. Adjusted spacing between box art sides. Added drop shadow to main panel.

--- client/src/search.js
+++ client/src/search.js
@@ -2,8 +2,6 @@
 
 angular.module('gameBoxArtSearchApp')
   .controller('SearchCtrl', function($scope, $location, GamesDbNet) {
-    $scope.welcome = 'Welcome to Video Game Box Art Search!';
-    $scope.buttonText = 'This is your Button';
     $scope.submit = function(){
       GamesDbNet.searchBoxArtURLs($scope.searchTerm)
       .then(function(){
